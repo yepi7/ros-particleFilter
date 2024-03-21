742e5723634b2e14d2789edd63f5cc0931e85d97
yepi7 <giussepi97@gmail.com>
2024-03-21 02:49:33 +0100

    "Agregada la primera parte del matching"

--- src/firstProj/src/firstProj_node.cpp
+++ src/firstProj/src/firstProj_node.cpp
@@ -19,14 +19,14 @@
 
 #include <SFML/Graphics.hpp>
 
+/* 
+Mapa de 400 (width) x 300 (height). Tiene paredes, por lo que asumiremos que ocupan 10 pixeles de grosor.
+*/
+
 namespace bg = boost::geometry;
 typedef bg::model::point<float, 2, bg::cs::cartesian> point;
 typedef bg::model::box<point> box;
 typedef bg::model::linestring<point> linestring;
-
-// Prototipos de funciones
-void calculateDistance(point sonar, std::vector<box> obstaculos, int laserRadius);
-void particlesGeneration (int numberOfParticles, Particle arrayOfParticles[]);
 
 // Definicion de una Particula. No se agrega la distancia porque NO forma parte de la particula.
 // Es un valor que se obtiene tras su creacion.
@@ -34,6 +34,12 @@ struct Particle {
     point position;
     float direction;
 };
+
+// Prototipos de funciones
+void calculateDistance(point sonar, std::vector<linestring> obstaculos, int laserRadius, float arrayOfDistances[]);
+void particlesGeneration (int numberOfParticles, Particle arrayOfParticles[]);
+void creacionObstaculos(std::vector<linestring>& obstaculos);
+float distanciaEuclidea (float *array1, float *array2, int longitud);
 
 // ========================================================
 // Funcion main
@@ -46,37 +52,75 @@ int main(int argc, char** argv){
     // ========================================================
 
     // Definicion de parametros
-    const int laserRadius = 300;
-    const int numberOfParticles = 1;
-    point sonarPositions[3];
+    const int laserRadius = 250;
+    const int numberOfParticles = 100;
+    std::vector<linestring> obstaculos;
+    point sonarPositions[6];
     Particle arrayOfParticles [numberOfParticles]; // Array con Particulas aleatorias
-    // point arrayOfParticles [numberOfParticles] = {point(0.0,0.0)};
-    float distancesOfPartciles [numberOfParticles][360]; // Guarda las distancias de las particulas en cada iteracion
-    
-    // Simula el movimiento del robot
-	sonarPositions[0] = point(0.0,0.0);
-    sonarPositions[0] = point(0.5,0.0);
-    sonarPositions[0] = point(1.0,0.0);
+    float distanceComparison [numberOfParticles]; // Array con distancia euclidea
 
-	// Definicion de los obstaculos. De momento son 3.
-    std::vector<box> obstaculos = {
-        // box(point(-5, -5), point(-3, 0)),
-        // box(point(2, 1), point(4, 3)),
-        // box(point(5, -2), point(7, 1))
-        box(point(-10, 20), point(10, 40)), // (0, 30)
-        box(point(40, -15), point(60, 15)), // (50, 0)
-        box(point(-115, -10), point(-85, 10)) // (-100, 0)
-    };
+    // Creacion del mapa
+    creacionObstaculos(obstaculos);
+
+    // Matriz de particulas y distancias
+    float distancesOfParticles [numberOfParticles][360]; // Guarda las distancias de las particulas en cada iteracion
+    // Matriz de distancias reales del robot
+    float realDistancesOfRobot [6][360]; // Se crean 6 posiciones. Estan hardcodeadas. Modificar despues.
     
+    // Simula el movimiento del robot. Posiciones reales
+	sonarPositions[0] = point(0.0,0.0);
+    sonarPositions[1] = point(0.5,0.0);
+    sonarPositions[2] = point(1.0,0.0);
+    sonarPositions[3] = point(1.5,0.0);
+    sonarPositions[4] = point(2,0.0);
+    sonarPositions[5] = point(2.5,0.0);
+
+
+    // Obtenemos las distancias desde las posiciones reales. Seran las distancias reales (medidas con sensor).
+    for (int i=0;i<6;i++){
+        calculateDistance(sonarPositions[i],obstaculos,laserRadius,realDistancesOfRobot[i]);
+        //// Para visualizar la matriz de distancias 
+        // std::cout << "Fila: " << i <<":"<< std::endl;
+        // for (int j=0;j<360;j++){
+        //     std::cout << realDistancesOfRobot[i][j] << " ";
+        // }
+        // std::cout << std::endl;
+    }
+    
+    // Genera las particulas y las guarda en arrayOfPartciles
+    particlesGeneration(numberOfParticles, arrayOfParticles);
+
+    // Calculamos las distancias de las particulas a los obstaculos
+    for (int i=0; i<numberOfParticles; i++){
+        calculateDistance(arrayOfParticles[i].position,obstaculos,laserRadius,distancesOfParticles[i]);
+        //// Para visualizar la matriz de distancias 
+        // std::cout << "Particula: " << i <<"=("<< bg::get<0>(arrayOfParticles[i].position)<<","<<bg::get<1>(arrayOfParticles[i].position)<<")"<<std::endl;
+        // for (int j=0;j<360;j++){
+        //     std::cout << distancesOfParticles[i][j] << " ";
+        // }
+        // std::cout << std::endl;
+    }
+    
+    // Calculamos la diferencia entre cada distancia en funcion del angulo.
+    // En este caso se comparara con la primera particula "realDistancesOfRobot[0]"
+    for (int i=0;i<numberOfParticles;i++){
+        distanceComparison[i] = distanciaEuclidea(realDistancesOfRobot[0],distancesOfParticles[i],360);
+        // Para visualizar la matriz de distancias 
+        std::cout << "Particula: " << i <<"=("<< bg::get<0>(arrayOfParticles[i].position)<<","<<bg::get<1>(arrayOfParticles[i].position)<<")"<<std::endl;
+        std::cout << distanceComparison[i] << std::endl;
+    }
+    
+    // Para asignar un PESO, dividir entre la distancia maxima posible, es decir, entre sqrt(200^2 + 150^2).
+    
+
     // ========================================================
     // Comienzo del programa
     // ========================================================
     
 
-    // Genera las particulas y las guarda en arrayOfPartciles
-    particlesGeneration(numberOfParticles, arrayOfParticles);
 
-	// Calculamos las distancias de las particulas a los obstaculos
+    /* arrayOfParticles[0].position = point(0,0);
+    
 	for (int i=0; i<numberOfParticles; i++){
 		std::cout << "-----------------------Comienzo particula-----------------------------" << std::endl;
         std::cout << "POSICION:"<< bg::get<0>(arrayOfParticles[i].position)<<"," <<bg::get<1>(arrayOfParticles[i].position) << std::endl;
@@ -84,12 +128,13 @@ int main(int argc, char** argv){
         calculateDistance(arrayOfParticles[i].position, obstaculos, laserRadius);
         
         std::cout << "-----------------------Fin Particula-----------------------------" << std::endl;
-	}
+	} */
 
     // ========================================================
     // Graficacion de los punntos
     // ========================================================
-    sf::RenderWindow window(sf::VideoMode(400,300), "Puntos con SFML");
+    
+    /* sf::RenderWindow window(sf::VideoMode(400,300), "Puntos con SFML");
 
     // Define algunos puntos como círculos pequeños
     sf::CircleShape particles[numberOfParticles];
@@ -145,9 +190,9 @@ int main(int argc, char** argv){
         for(int i=0; i<numberOfObstacles; i++){
             window.draw(obstacles[i]);
         }
-
-        window.display(); // Muestra el contenido de la ventana
-    }
+        // Muestra el contenido de la ventana
+        window.display(); 
+    } */
 
 	return 0;
 }
@@ -156,8 +201,12 @@ int main(int argc, char** argv){
 // Definicion de funciones externas
 // ========================================================
 
+/* Revisar, no obtieene distancias correctas con objetos tipo box, pero si con linestring. Las paredes estan
+    ya puestas como linestring */
+
 // No es necesario poner obstaculos como parametro, ya que el mapa es conocido. Puede ir en el main.
-void calculateDistance(point sonar, std::vector<box> obstaculos, int laserRadius){
+// La funcion calcula la distancia en funcion del radio del laser.
+void calculateDistance(point sonar, std::vector<linestring> obstaculos, int laserRadius, float arrayOfDistances[]){
 	for (int angle = 0; angle < 360; angle += 1) {
         float rad = angle * M_PI / 180.0;
         point direccion(std::cos(rad) * laserRadius, std::sin(rad) * laserRadius);
@@ -165,8 +214,10 @@ void calculateDistance(point sonar, std::vector<box> obstaculos, int laserRadius
         linestring rayo;
         rayo.push_back(sonar);
         rayo.push_back(direccion);
+
         // Buscamos si existen intersecciones
         std::vector<linestring> intersecciones;
+        // Busca intersecciones en los 360 grados. Si hay obstaculo, habra intersecccion.
         for (const auto& obstaculo : obstaculos) {
             bg::intersection(rayo, obstaculo, intersecciones);
         }
@@ -178,12 +229,14 @@ void calculateDistance(point sonar, std::vector<box> obstaculos, int laserRadius
                     // Calcula la distancia del sonar al primer punto de intersección encontrado.
                     //Las distancias se guardan en la variable "distancia"
                     float distancia = bg::distance(sonar, interseccion.front());
-                    std::cout << "Ángulo " << angle << " grados: Obstáculo detectado a " << distancia << " unidades." << std::endl;
+                    arrayOfDistances[angle] = distancia; 
+                    // std::cout << "Ángulo " << angle << " grados: Obstáculo detectado a " << distancia << " unidades." << std::endl;
                     break; // Supone que solo nos interesa el primer obstáculo detectado
                 }
             }
         } else {
-            std::cout << "Ángulo " << angle << " grados: Sin obstáculo." << std::endl;
+            // std::cout << "Ángulo " << angle << " grados: Sin obstáculo." << std::endl;
+            arrayOfDistances[angle] = std::numeric_limits<float>::infinity(); // Se asigna un numero muy grande ya que se buscara el minimo.
         }
     }
 }
@@ -201,4 +254,34 @@ void particlesGeneration (int numberOfParticles, Particle arrayOfParticles[]){
     arrayOfParticles[i].position  = point(width_distribution(motor),height_distribution(motor));
     arrayOfParticles[i].direction = direction_distribution(motor)*M_PI;
     }
+}
+
+void creacionObstaculos(std::vector<linestring>& obstaculos){
+    // Definición de las paredes del mapa como linestrings
+    linestring pared_izquierda;
+    pared_izquierda.push_back(point(-200, -150)); // Vértice inferior izquierdo
+    pared_izquierda.push_back(point(-200, 150)); // Vértice superior izquierdo
+
+    linestring pared_derecha;
+    pared_derecha.push_back(point(200, -150)); // Vértice inferior derecho
+    pared_derecha.push_back(point(200, 150)); // Vértice superior derecho
+
+    linestring pared_superior;
+    pared_superior.push_back(point(-200, 150)); // Vértice superior izquierdo
+    pared_superior.push_back(point(200, 150)); // Vértice superior derecho
+
+    linestring pared_inferior;
+    pared_inferior.push_back(point(-200, -150)); // Vértice inferior izquierdo
+    pared_inferior.push_back(point(200, -150)); // Vértice inferior derecho
+
+    // Vector que contiene las paredes del mapa
+    obstaculos = {pared_izquierda, pared_derecha, pared_superior, pared_inferior};
+}
+
+float distanciaEuclidea (float *array1, float *array2, int longitud){
+    float distancia = 0.0;
+    for (int i=0; i<longitud; i++){
+        distancia += pow(array2[i] - array1[i], 2);
+    }
+    return sqrt(distancia);
 }
